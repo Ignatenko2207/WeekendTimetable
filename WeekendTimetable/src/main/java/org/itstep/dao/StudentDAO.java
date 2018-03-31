2193bf9c27b3b5c717f044faf43dcfb65ee14300
Oleg <Oleg@DESKTOP-8E2A3QL.mshome.net>
2018-03-31 19:10:05 +0300

    Add method in StudentDAO

--- WeekendTimetable/src/main/java/org/itstep/dao/StudentDAO.java
+++ WeekendTimetable/src/main/java/org/itstep/dao/StudentDAO.java
@@ -1,5 +1,8 @@
 package org.itstep.dao;
 
+import java.util.List;
+
+import org.itstep.model.Group;
 import org.itstep.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentDAO extends JpaRepository<Student, String> {
 
-	
+	List<Student> FindAllByGroup(Group group);
 }
