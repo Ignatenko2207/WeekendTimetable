a8e750175cf42e3a8ee0a2efe22251f0559a4c66
maxna <maxna@172.31.2.214>
2018-03-31 19:48:57 +0300

    Add TeacherService started Implementation

---
+++ WeekendTimetable/src/main/java/org/itstep/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,15 @@
+package org.itstep.service.impl;
+
+import org.itstep.dao.TeacherDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TeacherServiceImpl {
+	
+	@Autowired
+	TeacherDAO TeacherDao;
+	
+	
+	
+	
+	
+}
