4efcfd00c6cc1e214be3a96d7a63be5d6d4d1b62
maxna <maxna@172.31.3.18>
2018-04-07 16:21:25 +0300

    Teacher Service Implementation realization added Teacher controller requestparam fixed Teacher POJO fixed

--- WeekendTimetable/src/main/java/org/itstep/model/Teacher.java
+++ WeekendTimetable/src/main/java/org/itstep/model/Teacher.java
@@ -1,21 +1,33 @@
 package org.itstep.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
+@Table( name = "teachers")
 public class Teacher {
-
+	
+	@Id
+	@Column(name = "login")
 	private String login;
-
+	
+	@Column(name = "password")
 	private String password;
 
+	@Column(name = "firstname")
 	private String firstName;
-
-	private String secondName;
-
-	private UserRole role;
 	
+	@Column(name = "secondname")
+	private String secondName;
+	
+	@ManyToOne(targetEntity = Subject.class)
 	private Subject subject;
 }
