fe8801860240df42ae76bf264cd9b36e7a5b1bc1
Oleg <pora93@gmail.com>
2018-04-07 17:18:55 +0300

    Edit Student class

--- WeekendTimetable/src/main/java/org/itstep/model/Student.java
+++ WeekendTimetable/src/main/java/org/itstep/model/Student.java
@@ -1,21 +1,33 @@
 package org.itstep.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
+@Table( name = "students")
 public class Student {
 
+	@Id
+	@Column(name = "login")
 	private String login;
 	
+	@Column(name = "password")
 	private String password;
 	
+	@Column(name = "firstname")
 	private String firstName;
 	
+	@Column(name = "secondname")
 	private String secondName;
 	
-	private UserRole role;
-	
+	@ManyToOne(targetEntity = Group.class)
 	private Group group;
 }
