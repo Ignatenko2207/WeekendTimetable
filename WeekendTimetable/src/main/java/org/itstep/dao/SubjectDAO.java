fd86d80e1081412a91e370295d2fc42429eb894b
student <student@K05-08.lan.itstep.kiev.ua>
2018-04-14 16:25:24 +0300

    conflict

--- WeekendTimetable/src/main/java/org/itstep/dao/SubjectDAO.java
+++ WeekendTimetable/src/main/java/org/itstep/dao/SubjectDAO.java
@@ -39,7 +39,7 @@ public class SubjectDAO {
 		}
 		return null;
 	}
-	Subject uppdate(Subject subject) {
+	Subject update(Subject subject) {
 		
 		if(get(subject.getId()) != null )
 		{
@@ -55,6 +55,7 @@ public class SubjectDAO {
 			
 			return subject;
 		}
+		return null;
 	}
 	
 	Subject get(Integer id) {
